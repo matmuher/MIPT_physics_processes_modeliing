37645ee8b85cd81f493d07b3d9d2f9b13003af39
matmuher <barbusas.mail@gmail.com>
2023-10-05 00:21:58 +0300

    Refactor plotter to easily add new method and plot scenarios

--- week4/plotter.py
+++ week4/plotter.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import subprocess
 
+
 def read_solver_data(file_name):
 
 	xv_dtype = np.dtype([('x', np.float32), ('v', np.float32)])
@@ -9,49 +10,122 @@ def read_solver_data(file_name):
 
 	return xv_data
 
-def plot_solver_data(config_name):
+class MethodData:
+
+	def __init__(self, method_name, binary_file, marker):
+
+		self.binary_file = binary_file
+		self.name = method_name
+		self.marker = marker
+
+		self.data = read_solver_data(binary_file);
+
+	def plot_solution(self, ax):
+
+		t_sample_list = range(0, self.data.size)
+		ax.plot(t_sample_list, self.data['x'], label = self.name + ' x', marker = self.marker)
+		ax.plot(t_sample_list, self.data['v'], label = self.name + ' v', marker = self.marker)
+
+	def plot_energy(self, ax):
+
+		t_sample_list = range(0, self.data.size)
+		ax.plot(	t_sample_list,
+					self.data['x'] * self.data['x'] + self.data['v'] * self.data['v'],
+					label = self.name + ' x',
+					marker = self.marker)
+
+	def plot_phase_diagram(self, ax):
+
+		ax.plot(self.data['x'], self.data['v'], label = self.name + ' v', marker = self.marker)
+
+def plot_solution(ax, methods):
+
+	ax.set_title('Solutions comparison')
+	ax.set_xlabel('time samples')
+	ax.set_ylabel('x & v')	
+
+	for method in methods:
+
+		method.plot_solution(ax)
+
+def plot_phase_diagram(ax, methods):
+
+	ax.set_title('Phase diagrams comparison')
+	ax.set_xlabel('x')
+	ax.set_ylabel('v')
+
+	for method in methods:
+
+		method.plot_phase_diagram(ax)
+
+def plot_energy(ax, methods):
+
+	ax.set_ylabel('System energy comparison')
+	ax.set_xlabel('time samples')
+	ax.set_ylabel('energy')	
+
+	for method in methods:
+
+		method.plot_energy(ax)
+
+class PlotFunction:
+
+	def __init__(self, name, function):
+
+		self.call = function
+		self.name = name
+
+def save_plot_to_picture(config_name, plot_function, methods):
 
 	fig = plt.figure(figsize = (10, 10))
 	ax = plt.gca()
 
-	analytical_xv_data = read_solver_data('analytical_output.bin')
-	euler_xv_data = read_solver_data('euler_output.bin')
-	heun_xv_data = read_solver_data('heun_output.bin')
+	plot_function.call(ax, methods)
 
-	t_sample_list = range(0, euler_xv_data.size)
-
-	ax.plot(t_sample_list, analytical_xv_data['x'], label = 'analytical x') # wrap plotting rouitine
-	ax.plot(t_sample_list, analytical_xv_data['v'], label = 'analytical v') # into function / class
-
-	ax.plot(t_sample_list, euler_xv_data['x'], label = 'euler x')
-	ax.plot(t_sample_list, euler_xv_data['v'], label = 'euler v')
-
-	ax.plot(t_sample_list, heun_xv_data['x'], label = 'heun x')
-	ax.plot(t_sample_list, heun_xv_data['v'], label = 'heun v')
-
-	ax.set_title(config_name)
 	ax.legend()
+	fig.savefig(f'{config_name}_{plot_function.name}.svg');
 
-	fig.savefig(f'{config_name}.svg')	
+def save_plots_to_pictures(config_name, plot_functions, methods):
+
+	for plot_function in plot_functions:
+
+		save_plot_to_picture(config_name, plot_function, methods)
+
+def plot_solver_data(config_name):
+
+	fig, axs = plt.subplots(1, 3, figsize = (30, 10))
+
+	methods = 	[	
+					MethodData('analytic', 'analytical_output.bin', '|'),
+					MethodData('heun', 'heun_output.bin', 'o'),
+					MethodData('euler', 'euler_output.bin', '^')
+				]
+	
+	plot_functions = 	[
+						PlotFunction('solution', plot_solution),
+						PlotFunction('energy', plot_energy),
+						PlotFunction('phase_diagram', plot_phase_diagram)
+						]
+
+	save_plots_to_pictures(config_name, plot_functions, methods)
+
+class ConfigInfo:
+
+	def __init__(self, config_name, config_path):
+
+		self.name = config_name
+		self.path = config_path
 
 if __name__ == '__main__':
 
 	executable_name = './solver.exe'
 	
-	few_samples_config_path = 'few_samples_config.json'
-	few_samples_config_name = 'few_samples'
+	configs = 	[
+				ConfigInfo('few_samples', 'few_samples_config.json'),
+				ConfigInfo('many_samples', 'many_samples_config.json')
+				]
 
-	many_samples_config_path = 'many_samples_config.json'
-	many_samples_config_name = 'many_samples'
+	for config in configs:
 
-	subprocess.run([executable_name, few_samples_config_path])
-	plot_solver_data(few_samples_config_name)
-
-	subprocess.run([executable_name, many_samples_config_path])
-	plot_solver_data(many_samples_config_name)
-
-	# it's better to open them not in sequential way
-	# but in parallel, or plot them on the same figure
-	subprocess.run(['eog', few_samples_config_name + '.svg'])
-	subprocess.run(['eog', many_samples_config_name + '.svg'])
-
+		subprocess.run([executable_name, config.path])
+		plot_solver_data(config.name)
