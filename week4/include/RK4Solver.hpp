eba58d14607e5cdc0754fed8f5cbd2e866fc21e4
matmuher <barbusas.mail@gmail.com>
2023-10-28 13:54:32 +0300

    Templatize Rk4

--- week4/include/RK4Solver.hpp
+++ week4/include/RK4Solver.hpp
@@ -4,38 +4,53 @@
 
 namespace hos
 {
-	class RK4Solver : public Solver
+	template<class T, int N>
+	class RK4SolverT : public SolverT<T, N>
 	{
+		using SolverT<T, N>::diffSystem_;
+
+		using SolverT<T, N>::deltaT;
+
+		using SolverT<T, N>::tRange_;
+
+		using SolverT<T, N>::startConds_;
+
 		void fillSolutions() override;
 
 	public:
 
-		RK4Solver(HarmonicOscillator hOs, Vec2 startConds, Range tRange, const std::string& fileName)
+		RK4SolverT(	DiffEqSystem<T, N> diffSystem,
+					vec<T, N> startConds,
+					RangeT<T> tRange,
+					const std::string& fileName)
 		:
-			Solver{hOs, startConds, tRange, fileName}
+			SolverT<T, N>{diffSystem, startConds, tRange, fileName}
 		{}
 	};
 
-	void RK4Solver::fillSolutions()
+	template<class T, int N>
+	void RK4SolverT<T, N>::fillSolutions()
 	{
-		Vec2 prevSolution = startConds_;
+		vec<T, N> prevSolution = startConds_;
 
 		for (float_t t = tRange_.t1; t < tRange_.t2; t += deltaT)
 		{
-			Vec2 k1 = diffSystem_.f(prevSolution);
+			vec<T, N> k1 = diffSystem_.f(prevSolution);
 
-			Vec2 k2 = diffSystem_.f(prevSolution + deltaT / 2 * k1);
+			vec<T, N> k2 = diffSystem_.f(prevSolution + deltaT / 2 * k1);
 
-			Vec2 k3 = diffSystem_.f(prevSolution + deltaT / 2 * k2);
+			vec<T, N> k3 = diffSystem_.f(prevSolution + deltaT / 2 * k2);
 
-			Vec2 k4 = diffSystem_.f(prevSolution + deltaT * k3);
+			vec<T, N> k4 = diffSystem_.f(prevSolution + deltaT * k3);
 
 			// y_{k+1} = y_{k} + deltaT / 2 * (f(y_{k} + f(y*_{k+1}))
-			Vec2 currSolution = prevSolution + deltaT * (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+			vec<T, N> currSolution = prevSolution + deltaT * (k1 + 2 * k2 + 2 * k3 + k4) / 6;
 
-			addSolution(currSolution);
+			SolverT<T, N>::addSolution(currSolution);
 
 			prevSolution = currSolution;
 		}
 	}
+
+	using RK4Solver = RK4SolverT<float, 2>;
 };
